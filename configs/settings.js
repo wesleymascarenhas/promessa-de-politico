f406ad271344eaa1ae31293515908365a89c7027
Sandro Simas <sandro.csimas@gmail.com>
2014-06-16 17:47:23 -0300

    Removing fix value from publicPath in settings.js

--- configs/settings.js
+++ configs/settings.js
@@ -1,7 +1,8 @@
-var _nodeEnv = process.env.NODE_ENV || 'dev',
+var path = require('path'),
+    _nodeEnv = process.env.NODE_ENV || 'dev',
     _nodeIP = process.env.NODE_IP || '127.0.0.1',
     _nodePort = process.env.NODE_PORT || 3000,
-    _publicPath = '/home/sandro/workspace/promessa-de-politico/public',
+    _publicPath = path.join(path.dirname(process.mainModule.filename), "./public"),
     _database = {
       dev: {
         dialect: 'mysql',
@@ -16,23 +17,25 @@ var _nodeEnv = process.env.NODE_ENV || 'dev',
       dev: {
         clientID: '235774129950285',
         clientSecret: '329ada86e67ca322ae5c56af8f5a167d',
-        callbackURL: 'http://www.promessadepolitico.com.br:3000/auth/facebook/callback'
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/facebook/callback'
       }
     },
     _twitter = {
       dev: {
         clientID: 'SXAZVFlQalUVp1hZwFX2gYK0l',
         clientSecret: '0gmtYviV8VpASXmtLG5cLB41jI8ofcAVIP7Wfy16tIGoUXAGmX',
-        callbackURL: 'http://www.promessadepolitico.com.br:3000/auth/twitter/callback'
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/twitter/callback'
       }
     },
     _google = {
       dev: {
         clientID: '37782263354-thqn9d44li64finmomvf5es38h0f4r73.apps.googleusercontent.com',
         clientSecret: 'x1TrI6h07SjHyWzQA_Tjpsk1',
-        callbackURL: 'http://www.promessadepolitico.com.br:3000/auth/google/callback'
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/google/callback'
       }
     }
+
+console.log("PUBLIC" + _publicPath)
 
 module.exports = {
   nodeEnv: _nodeEnv,
