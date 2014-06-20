b8f13d7f2569a764a23d0eff6cf580ed941dc345
Sandro Simas <sandro.csimas@gmail.com>
2014-06-19 23:18:46 -0300

    Adding prod environment to settings config

--- configs/settings.js
+++ configs/settings.js
@@ -2,7 +2,7 @@ var path = require('path'),
     _nodeEnv = process.env.NODE_ENV || 'dev',
     _nodeIP = process.env.NODE_IP || '127.0.0.1',
     _nodePort = process.env.NODE_PORT || 3000,
-    _publicPath = path.join(path.dirname(process.mainModule.filename), "./public"),
+    _publicPath = path.join(path.dirname(process.mainModule.filename), './public'),
     _email = {
       contact: 'sandro.csimas@gmail.com',
       apiKey: 'key-8k25kie96mv1wpdi01gnqu47-nos-0y1',
@@ -16,10 +16,23 @@ var path = require('path'),
         password: 'sandroa1s2d3',
         schema: 'promessa_de_politico',
         charset: 'utf8',
+      },
+      prod: {
+        dialect: 'mysql',
+        host: '162.243.200.220',
+        user: 'root',
+        password: 'roota1s2d3',
+        schema: 'promessa_de_politico',
+        charset: 'utf8'
       }
     },
     _facebook = {
       dev: {
+        clientID: '235774129950285',
+        clientSecret: '329ada86e67ca322ae5c56af8f5a167d',
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/facebook/callback'
+      },
+      prod: {
         clientID: '235774129950285',
         clientSecret: '329ada86e67ca322ae5c56af8f5a167d',
         callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/facebook/callback'
@@ -30,10 +43,20 @@ var path = require('path'),
         clientID: 'SXAZVFlQalUVp1hZwFX2gYK0l',
         clientSecret: '0gmtYviV8VpASXmtLG5cLB41jI8ofcAVIP7Wfy16tIGoUXAGmX',
         callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/twitter/callback'
+      },
+      prod: {
+        clientID: 'SXAZVFlQalUVp1hZwFX2gYK0l',
+        clientSecret: '0gmtYviV8VpASXmtLG5cLB41jI8ofcAVIP7Wfy16tIGoUXAGmX',
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/twitter/callback'
       }
     },
     _google = {
       dev: {
+        clientID: '37782263354-thqn9d44li64finmomvf5es38h0f4r73.apps.googleusercontent.com',
+        clientSecret: 'x1TrI6h07SjHyWzQA_Tjpsk1',
+        callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/google/callback'
+      },
+      prod: {
         clientID: '37782263354-thqn9d44li64finmomvf5es38h0f4r73.apps.googleusercontent.com',
         clientSecret: 'x1TrI6h07SjHyWzQA_Tjpsk1',
         callbackURL: 'http://www.promessadepolitico.com.br:' + _nodePort + '/auth/google/callback'
